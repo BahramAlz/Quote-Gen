09e064ff429ac39926a8537a8a28c19062d9ad17
bahram alzuhairi <bahram.alzuhairi@hyperisland.se>
2022-11-04 11:38:18 +0100

    testapi

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click", getQuote);
 
 async function getQuote() {
-    const res = await fetch("http://api.quotable.io/random")
+    const res = await fetch("https://type.fit/api/quotes")
     const data = await res.json();
 
-    quote.innerHTML = `"${data.content}"`; 
+    quote.innerHTML = `"${data.text}"`; 
     author.innerHTML = data.author;
 }
